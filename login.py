f78d934133053e5e7cb8d50174342c40f8c732ff
sameckmatheus <sameckmatheuspro@gmail.com>
2023-06-23 16:41:11 -0300

    Setup Ilustrations (bitmap/image-frame)

--- login.py
+++ login.py
@@ -6,16 +6,21 @@ customtkinter.set_default_color_theme("dark-blue")
 
 # window-box setup
 window = customtkinter.CTk()
-window.geometry("700x500")
+window.geometry("700x400")
 window.title("Login")
-window.iconbitmap("imges/favicon.ico")
+window.iconbitmap("favicon.ico")
 window.resizable(False, False)
 
 # img box-selector
-img = PhotoImage(file="images/background.png")
+img = PhotoImage(file="background.png")
 Label_img = customtkinter.CTkLabel(master=window, image=img)
 Label_img.place(x=5, y=65)
 
+# frame-box in window-box setup
+frame = customtkinter.CTkFrame(master=window, width=350, height=395)
+frame.pack(side=RIGHT)
+
+# login elements
 
 
 window.mainloop()
