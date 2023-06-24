a4b674d55f432445126ba95177cec73a9bf622e6
sameckmatheus <sameckmatheuspro@gmail.com>
2023-06-23 21:39:11 -0300

    Setup Input+Label Area

--- login.py
+++ login.py
@@ -22,9 +22,17 @@ frame = customtkinter.CTkFrame(master=window, width=350, height=395)
 frame.pack(side=RIGHT)
 
 # frame widgets
-label = customtkinter.CTkLabel(master=frame, text="Sistema de Login")
+label = customtkinter.CTkLabel(master=frame, text="Sistema de Login", text_font=("JetBrains mono", 20))
 label.place(x=25, y=5)
 
-input1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+username_input = customtkinter.CTkEntry(master=frame, 
+    placeholder_text="Username", 
+    width=300, 
+    text_font=("JetBrains mono", 14)).place(x=25, y=105)
+
+username_label = customtkinter.CTkLabel(master=frame, 
+    text="*Preencha o campo acima para continuar",
+    text_color="blue", 
+    text_font=("JetBrains mono", 8)).place(x=25, y=135)
 
 window.mainloop()                                                
