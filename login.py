6ac131a0223f752717e41b959d1c630ee5648828
sameckmatheus <sameckmatheuspro@gmail.com>
2023-06-28 00:15:04 -0300

    Update login.py

--- login.py
+++ login.py
@@ -2,11 +2,11 @@ import customtkinter as slva # pkg importation
 from tkinter import * # pkg call
 from PIL import Image, ImageTk # pkg mges support
 
-ve.set_appearance_mode("dark")
-ve.set_default_color_theme("dark-blue")
+slva.set_appearance_mode("dark")
+slva.set_default_color_theme("dark-blue")
 
 # window-box setup
-window = ve.CTk()
+window = slva.CTk()
 window.geometry("700x400")
 window.title("Login")
 window.iconbitmap("favicon.ico")
@@ -14,45 +14,45 @@ window.resizable(False, False)
 
 # img box-selector
 img = PhotoImage(file="images/log.png")
-Label_img = ve.CTkLabel(master=window, image=img)
+Label_img = slva.CTkLabel(master=window, image=img)
 Label_img.place(x=25, y=65)
 Label_img.configure(text='')
 
-label_title = ve.CTkLabel(master=window, text="Bem Vindos!", font=("JetBrains mono", 20)).place(x=105, y=10)
+label_title = slva.CTkLabel(master=window, text="Bem Vindos!", font=("JetBrains mono", 20)).place(x=105, y=10)
 
 # frame-box in window-box setup
-frame = ve.CTkFrame(master=window, width=350, height=396)
+frame = slva.CTkFrame(master=window, width=350, height=396)
 frame.pack(side=RIGHT)
 
 # frame widgets
-label = ve.CTkLabel(master=frame, text="Sistema de Login", font=("JetBrains mono", 20))
+label = slva.CTkLabel(master=frame, text="Sistema de Login", font=("JetBrains mono", 20))
 label.place(x=70, y=5)
 
-username_input = ve.CTkEntry(master=frame, 
+username_input = slva.CTkEntry(master=frame, 
     placeholder_text="Username", 
     width=300, 
     font=("JetBrains mono", 14)).place(x=25, y=105)
 
-username_label = ve.CTkLabel(master=frame, 
+username_label = slva.CTkLabel(master=frame, 
     text="*Preencha o campo acima para continuar", 
     text_color="orange", 
     font=("JetBrains mono", 8)).place(x=25, y=135)
 
-pass_input = ve.CTkEntry(master=frame, 
+pass_input = slva.CTkEntry(master=frame, 
     placeholder_text="Password", 
     width=300, 
     font=("JetBrains mono", 14), show="°").place(x=25, y=175)
 
-pass_label = ve.CTkLabel(master=frame, 
+pass_label = slva.CTkLabel(master=frame, 
     text="*Preencha o campo acima para continuar", 
     text_color="orange", 
     font=("JetBrains mono", 8)).place(x=25, y=205)
 
-checkbox = ve.CTkCheckBox(master=frame, 
+checkbox = slva.CTkCheckBox(master=frame, 
     text="Lembre de mim!", 
     font=("JetBrains mono", 10)).place(x=25, y=245)
 
-button = ve.CTkButton(master=frame, 
+button = slva.CTkButton(master=frame, 
     text="Login", 
     width=300, 
     font=("JetBrains mono", 10)).place(x=25, y=305)
