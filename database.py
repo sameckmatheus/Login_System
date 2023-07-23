d991722c4514709d4bb9d8f3e46b226a58a0d661
sameckmatheus <sameckmatheuspro@gmail.com>
2023-07-22 23:49:32 -0300

    Update: Change Class -> SQLiteConnection

--- database.py
+++ database.py
@@ -18,18 +18,7 @@ class SQLiteConnection(DatabaseConnection):
     def connect(self):
         self.connection = sqlite3.connect(self.config.database_name)
 
-database_connection = sqlite3.Connection('database')
-cursor = database_connection.cursor()
-
-# database table with sqlite3
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        UserName TEXT NOT NULL,
-        Email TEXT NOT NULL,
-        Password TEXT NOT NULL,
-        ConfirmPassword TEXT NOT NULL        
-    )
-""")
-
-print("Successful Database Connection...")
+    def execute_query(self, query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        self.connection.commit()
