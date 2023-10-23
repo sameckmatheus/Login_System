91c3a5bc3800b3b7af88c72be7c7ea08d1193205
sameckmatheus <sameckmatheuspro@gmail.com>
2023-10-23 20:53:59 -0300

    chore: add project structure + prettier

--- login.py
+++ login.py
@@ -1,106 +1,122 @@
-import customtkinter as ctk
 from tkinter import *
-from PIL import Image, ImageTk
-import sqlite_database
 from tkinter import messagebox
+from typing import Any
+import customtkinter as ctk
 
 # window-box setup
 window = ctk.CTk()
 
-class Application():
+
+def screen():
+    window.geometry("700x400")
+    window.title("Login")
+    window.iconbitmap("favicon.ico")
+    window.resizable(False, False)
+
+
+class Application:
     def __init__(self):
-        self.window=window
+        self.window = window
         self.theme()
-        Application.screen()
-        Application.login_screen()
+        screen()
+        login_screen()
         window.mainloop()
-        
-    def theme(self):
+
+    @staticmethod
+    def theme():
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("dark-blue")
-        
-    def screen():
-        window.geometry("700x400")
-        window.title("Login")
-        window.iconbitmap("favicon.ico")
-        window.resizable(False, False)
 
-    def login_screen():
-        # img box-selector
-        img = PhotoImage(file="images/scene-3.png")
-        Label_img = ctk.CTkButton(master=window, image=img, text=None, hover=None, fg_color='transparent')
-        Label_img.place(x=-345, y=-260)
-        Label_img.configure(text='')
 
-        label_title = ctk.CTkLabel(master=window, text="Bem Vindos!", font=("JetBrains mono", 20)).place(x=105, y=10)
+def login_screen():
+    # img box-selector
+    img = PhotoImage(file="images/scene-3.png")
+    label_img = ctk.CTkButton(master=window, image=img, fg_color='transparent')
+    label_img.place(x=-345, y=-260)
+    label_img.configure(text='')
 
-        # frame-box to login
-        login_frame = ctk.CTkFrame(master=window, width=350, height=396)
-        login_frame.pack(side=RIGHT)
+    ctk.CTkLabel(master=window, text="Bem Vindos!", font=("JetBrains mono", 20)).place(x=105, y=10)
 
-        # frame widgets
-        label = ctk.CTkLabel(master=login_frame, text="Sistema de Login", font=("JetBrains mono", 20)).place(x=85, y=5)
+    # frame-box to login
+    login_frame: Any = ctk.CTkFrame(master=window, width=350, height=396)
+    login_frame.pack(side=RIGHT)
 
-        username_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Nome de usuário", width=300, font=("JetBrains mono", 14)).place(x=25, y=105)
-        username_label = ctk.CTkLabel(master=login_frame, text="* Preencha o campo acima para continuar", text_color="orange", font=("JetBrains mono", 10)).place(x=25, y=135)
+    # frame widgets
+    ctk.CTkLabel(master=login_frame, text="Sistema de Login", font=("JetBrains mono", 20)).place(x=85, y=5)
 
-        password_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Senha", width=300, font=("JetBrains mono", 14), show="°").place(x=25, y=175)
-        password_label = ctk.CTkLabel(master=login_frame, text="* Preencha o campo acima para continuar", text_color="orange", font=("JetBrains mono", 10)).place(x=25, y=205)
+    ctk.CTkEntry(master=login_frame, placeholder_text="Nome de usuário", width=300,
+                 font=("JetBrains mono", 14)).place(x=25, y=105)
+    ctk.CTkLabel(master=login_frame, text="* Preencha o campo acima para continuar",
+                 text_color="orange", font=("JetBrains mono", 10)).place(x=25, y=135)
 
-        checkbox = ctk.CTkCheckBox(master=login_frame, text="Lembre de mim!", font=("JetBrains mono", 10)).place(x=25, y=245)
+    ctk.CTkEntry(master=login_frame, placeholder_text="Senha", width=300,
+                 font=("JetBrains mono", 14), show="°").place(x=25, y=175)
+    ctk.CTkLabel(master=login_frame, text="* Preencha o campo acima para continuar",
+                 text_color="orange", font=("JetBrains mono", 10)).place(x=25, y=205)
 
-        def login():
-            message = messagebox.showinfo(title="Atenção!", message="Login realizado com sucesso!")
+    ctk.CTkCheckBox(master=login_frame, text="Lembre de mim!", font=("JetBrains mono", 10)).place(x=25,
+                                                                                                  y=245)
 
-            pass
+    def login():
+        messagebox.showinfo(title="Atenção!", message="Login realizado com sucesso!")
 
-        login_button = ctk.CTkButton(master=login_frame, text="Login", width=300, hover_color='#275fcf', font=("JetBrains mono", 10), command=login).place(x=25, y=285)
-        
-        register_span = ctk.CTkLabel(master=login_frame, text="Ainda não tem uma conta?", font=("JetBrains mono", 10)).place(x=25, y=325)
 
-        def Register_Screen():
-            # go to register
-            login_frame.pack_forget()
+    ctk.CTkButton(master=login_frame, text="Login", width=300, hover_color='#275fcf',
+                  font=("JetBrains mono", 10), command=login).place(x=25, y=285)
 
-            # frame-box to register
-            register_frame = ctk.CTkFrame(master=window, width=350, height=396)
-            register_frame.pack(side=RIGHT)
+    ctk.CTkLabel(master=login_frame, text="Ainda não tem uma contra?",
+                 font=("JetBrains mono", 10)).place(x=25, y=325)
 
-            label = ctk.CTkLabel(master=register_frame, text="Cadastre-se!", font=("JetBrains mono", 20)).place(x=105, y=5)
+    def register():
+        # go to register
+        login_frame.pack_forget()
 
-            span = ctk.CTkLabel(master=register_frame, text="* Os campos abaixo são obrigatórios.", font=("Jetbrains mono", 10), text_color="gray").place(x=25, y=65)
+        # frame-box to register
+        register_frame = ctk.CTkFrame(master=window, width=350, height=396)
+        register_frame.pack(side=RIGHT)
 
-            username_entry = ctk.CTkEntry(master=register_frame, placeholder_text="Digite seu nome de usuário", width=300, font=("JetBrains mono", 14)).place(x=25, y=105)
+        ctk.CTkLabel(master=register_frame, text="Cadastre-se!", font=("JetBrains mono", 20)).place(x=105,
+                                                                                                    y=5)
 
-            email_entry = ctk.CTkEntry(master=register_frame, placeholder_text="Digte seu email", width=300, font=("JetBrains mono", 14)).place(x=25, y=145)
+        ctk.CTkLabel(master=register_frame, text="* Os campos abaixo são obrigatórios.",
+                     font=("Jetbrains mono", 10), text_color="gray").place(x=25, y=65)
 
-            password_entry = ctk.CTkEntry(master=register_frame, placeholder_text="Crie uma senha", width=300, font=("JetBrains mono", 14), show="°").place(x=25, y=185)
+        ctk.CTkEntry(master=register_frame, placeholder_text="Digite seu nome de usuário",
+                     width=300, font=("JetBrains mono", 14)).place(x=25, y=105)
 
-            confirm_password_entry = ctk.CTkEntry(master=register_frame, placeholder_text="Confirme sua senha", width=300, font=("JetBrains mono", 14), show="°").place(x=25, y=225)
+        ctk.CTkEntry(master=register_frame, placeholder_text="Digte seu email", width=300,
+                     font=("JetBrains mono", 14)).place(x=25, y=145)
 
-            checkbox = ctk.CTkCheckBox(master=register_frame, text="Li e concordo.", font=("JetBrains mono", 10)).place(x=25, y=265)
+        ctk.CTkEntry(master=register_frame, placeholder_text="Crie uma senha", width=300,
+                     font=("JetBrains mono", 14), show="°").place(x=25, y=185)
 
-            def back():
-                # back to login
-                register_frame.pack_forget()
-                login_frame.pack(side=RIGHT)
+        ctk.CTkEntry(master=register_frame, placeholder_text="Confirme sua senha",
+                     width=300, font=("JetBrains mono", 14), show="°").place(x=25, y=225)
 
-                pass
+        ctk.CTkCheckBox(master=register_frame, text="Li e concordo.", font=("JetBrains mono", 10)).place(
+            x=25, y=265)
 
-            back_to_login = ctk.CTkButton(master=register_frame, text="Voltar para o login", hover_color='#275fcf', width=145, command=back).place(x=25, y=315)
+        def back():
+            # back to login
+            register_frame.pack_forget()
+            login_frame.pack(side=RIGHT)
 
-            # save user
-            def save_user():
-                message = messagebox.showinfo(title="Atenção!", message="Usuário cadastrado com sucesso!")
 
-                pass
-            
-            save_rg = ctk.CTkButton(master=register_frame, text="Cadastrar", fg_color='green', hover_color='#40A130', width=145, command=save_user).place(x=185, y=315)
+        ctk.CTkButton(master=register_frame, text="Voltar para o login", hover_color='#275fcf',
+                      width=145, command=back).place(x=25, y=315)
 
-            pass
+        # save user
+        def save_user():
+            messagebox.showinfo(title="Atenção!", message="Usuário cadastrado com sucesso!")
 
-        register_button = ctk.CTkButton(master=login_frame, text="Cadastre-se!", fg_color='green', hover_color='#40A130', width=150, command=Register_Screen , font=("JetBrains mono", 10)).place(x=175, y=325)
 
-        pass
+        ctk.CTkButton(master=register_frame, text="Cadastrar", fg_color='green', hover_color='#40A130',
+                      width=145, command=save_user).place(x=185, y=315)
 
-Application()                                              
+
+    ctk.CTkButton(master=login_frame, text="Cadastre-se!", fg_color='green',
+                  hover_color="#40A130", width=150, font=("JetBrains mono", 10),
+                  command=register).place(x=175, y=325)
+
+
+Application()
